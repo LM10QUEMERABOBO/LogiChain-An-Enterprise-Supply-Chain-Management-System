a49b528d353d9eb55797e04196da99880aa19e83
krish_soni <krishsoni198@gmail.com>
2025-09-07 14:59:13 +0530

    first commit

---
+++ backend/logichain/src/test/java/com/example/logichain/LogichainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.logichain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogichainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
